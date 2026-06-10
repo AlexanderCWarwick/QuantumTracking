Add heatmap plotting functionality. Correct bug in RBF formula.

tracking.py
@@ -7,10 +7,8 @@ def create_hits(N, x, m, c, sigma, truth_color):
     hits = (m*x + c) + np.random.normal(loc=0,scale=sigma,size=N)
     return hits, truth_labels
 
-def generate_lineparams(m_lowlimit, m_highlimit, c_lowlimit, c_highlimit):          #Generate model track parameters from uniform dist. Bounds of gradient
-    m = np.random.uniform(m_lowlimit, m_highlimit)                                  #is chosen small (-0.3,0.3) to small to model LHCb 'forward-type' interactions.
-    c = np.random.uniform(c_lowlimit, c_highlimit)  
-    return m, c
+def generate_lineparams(m_low, m_high, c_low, c_high):          #Generate model track parameters from uniform dist. Bounds of gradient chosen for forward-type interaction.
+    return np.random.uniform(m_low, m_high), np.random.uniform(c_low, c_high)
     
 def construct_toytracks(x,N,sigma,intsec):                              #Differetiate between intersecting or not with bool intsec. Tracks are particles hitting N 
     m1, c1 = generate_lineparams(-0.3,0.3,0,1)                          #equally spaced detectors. Model noise as gaussian, sd = sigma.
@@ -24,7 +22,7 @@ def construct_toytracks(x,N,sigma,intsec):                              #Differe
             else:
                 continue
     if intsec:
-        m1, c1 = generate_lineparams(-0.3,0.3,0,1)
+        m2, c2 = generate_lineparams(-0.3,0.3,0,1)
         
     track1, track1_labels = create_hits(N, x, m1, c1, sigma, 'red')
     track2, track2_labels = create_hits(N, x, m2, c2, sigma, 'blue')
@@ -38,12 +36,17 @@ def plot_toytracks(x, track1, track1_labels, track2, track2_labels, sigma, intse
     plt.title(f'Two track plot with intersection={intsec}. Noise standev.={sigma}')
     plt.grid(axis='x')
     plt.show()
-
-def construct_RBFmatrix(hit_coords, sigma):                                                #Radial Basis Function type similarity matrix. [dist_matrix]_ij = d(i,j) 
-    dist_matrix = cdist(hit_coords, hit_coords)                                            #with 0-5 being red truth labels and 6-11 being blue.
-    RBF_matrix = np.exp(-(dist_matrix)**2 / 2*(sigma)**2)
     
-    plt.imshow(RBF_matrix, cmap='viridis')
+def get_distmatrix(hit_coords): 
+    return cdist(hit_coords, hit_coords)
+
+def construct_RBFmatrix(hit_coords, sigma):                                                #Radial Basis Function type similarity matrix. [D]_ij = d(i,j) for normal euclid norm    
+    return np.exp(-(get_distmatrix(hit_coords))**2 / (2*(sigma)**2))  
+    
+def plot_matrix_heat_map(sim_matrix, matrix_type):
+    plt.imshow(sim_matrix, cmap='viridis')
+    plt.title(f'{matrix_type} Heat map')
+    plt.colorbar()
     plt.show()
     
 Num_hits = 6
@@ -55,4 +58,8 @@ track1, track1_labels, track2, track2_labels = construct_toytracks(x, Num_hits, 
 plot_toytracks(x, track1, track1_labels, track2, track2_labels, sigma_noise, intsec)
 
 hit_coords = np.column_stack([np.concatenate([x, x]),np.concatenate([track1, track2])])
-construct_RBFmatrix(hit_coords, 0.01)
+RBF_matrix = construct_RBFmatrix(hit_coords, 0.5)
+
+plot_matrix_heat_map(RBF_matrix, 'Radial Basis Function')
+plot_matrix_heat_map(get_distmatrix(hit_coords), 'Distance matrix')
+