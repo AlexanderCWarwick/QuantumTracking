d42febba251c6fa61e8475fad517130cc5955951
AlexanderCWarwick <Alexander.H.Cook@warwick.ac.uk>
2026-06-12 12:29:58 +0100

    Seperate KNN and RBF graph plots. Improve efficient and layout.

--- tracking.py
+++ tracking.py
@@ -12,33 +12,38 @@ def create_hits(N, x, m, c, sigma, truth_color):
     return hits, truth_labels
 
 
+
 def generate_lineparams(m_low, m_high, c_low, c_high):          
     return np.random.uniform(m_low, m_high), np.random.uniform(c_low, c_high)
 
     
-def construct_toytracks(x,N,sigma,intsec):
-    lims = (-0.5,0.5,0,1)           #Gradient bounds small in forward-type experiment.
-    m1, c1 = generate_lineparams(*lims)                          
+    
+def construct_toytracks(x,N,sigma_noise,allow_intersection):
+    track_lims = (-0.5, 0.5, 0, 1)                           #Gradient bounds small in forward-type experiment.
+    m1, c1 = generate_lineparams(*track_lims)                          
     m2, c2 = 0, 0
-    if not intsec:
+    
+    if allow_intersection:
+        m2, c2 = generate_lineparams(*track_lims)
+        
+    else:
         while True:
-            m2, c2 = generate_lineparams(*lims)
+            m2, c2 = generate_lineparams(*track_lims)
             
             while np.isclose(m1, m2, rtol=1e-8):
-                m2, _ = generate_lineparams(*lims)
+                m2, _ = generate_lineparams(*track_lims)
                 
             x_int = (c1 - c2) / (m2 - m1)
             if x_int > 1 or x_int < 0:
                 break 
             else:
                 continue
-    if intsec:
-        m2, c2 = generate_lineparams(*lims)
         
-    track1, track1_labels = create_hits(N, x, m1, c1, sigma, 'red')
-    track2, track2_labels = create_hits(N, x, m2, c2, sigma, 'blue')
+    track1, track1_labels = create_hits(N, x, m1, c1, sigma_noise, 'red')
+    track2, track2_labels = create_hits(N, x, m2, c2, sigma_noise, 'blue')
 
     return track1, track1_labels, track2, track2_labels
+
 
 
 def plot_toytracks(x, track1, track1_labels, track2, track2_labels, sigma, intsec):
@@ -51,12 +56,15 @@ def plot_toytracks(x, track1, track1_labels, track2, track2_labels, sigma, intse
     plt.show()
     
     
+    
 def get_distmatrix(hit_coords): 
     return cdist(hit_coords, hit_coords)
 
     
+    
 def construct_RBFmatrix(hit_coords, sigma_rbf):                                                  
     return np.exp(-(get_distmatrix(hit_coords))**2 / (2*(sigma_rbf)**2))  
+
 
     
 def plot_matrix_heat_map(sim_matrix, matrix_type):
@@ -66,60 +74,92 @@ def plot_matrix_heat_map(sim_matrix, matrix_type):
     plt.xlabel('Hit index')
     plt.savefig(f'plots/{matrix_type}_heat_map.png')
     plt.show()
+     
+     
     
-    
-def KNN(x, hit_coords, n, t1l, t2l):
+def contruct_KNN_matrix(x, hit_coords, n, t1l, t2l, sigma_rbf):
     nbrs = NearestNeighbors(n_neighbors=n, algorithm='ball_tree').fit(hit_coords)
-    distances, indices = nbrs.kneighbors(hit_coords)
     neighbour_matrix = nbrs.kneighbors_graph(hit_coords).toarray()
 
     knn_matrix = ((neighbour_matrix + neighbour_matrix.T)>0).astype(int)
+    return knn_matrix, nbrs
+
+        
+        
+def plot_KNN_graphrep(x, number_of_hits, nbrs, hit_coords):
+    H = nx.Graph()
+    hit_coords_dict = {i: tuple(hit_coords[i]) for i in range(len(hit_coords))}
     
-    distances = distances[:,1:]
+    _, indices = nbrs.kneighbors(hit_coords)
     indices = indices[:,1:]
-
-    pos = {i: tuple(hit_coords[i]) for i in range(len(hit_coords))}
-    G = nx.Graph()
-
-    for i in range(len(hit_coords)):
-        for j, d in zip(indices[i], distances[i]):
-            weight = np.exp(-d**2 / (2*(sigma_rbf)**2))
-            G.add_edge(i, j, weight=weight)
     
-    weights = np.array([G[u][v]["weight"] for u, v in G.edges()]) 
-
-    nx.draw_networkx_nodes(G, pos, node_size=200)
-    alphas = 0.05 + 0.95 * (weights - weights.min()) / (weights.max() - weights.min())
-
-    for (u, v), alpha in zip(G.edges(), alphas):
-        nx.draw_networkx_edges(G, pos, edgelist=[(u, v)], alpha=alpha, edge_color="black")
-    nx.draw_networkx_labels(G, pos)
+    for i in range(len(hit_coords)):
+        for j in indices[i]:
+            H.add_edge(i,j)
+           
+    nx.draw_networkx_labels(H, hit_coords_dict)
     
     for detector_x in x:
         plt.axvline(detector_x,linestyle='--',alpha=0.08)
-    
-    plt.title('Weighted kNN Graph')
-    plt.savefig('plots/kNN_Graph.png')
+        
+    nx.draw(H, hit_coords_dict)
+    plt.title('KNN_graph')
+    plt.savefig('plots/KNN_Graph.png')
     plt.show()
     
-    return knn_matrix
-
     
-num_hits = 6
-x = np.linspace(0,1,num_hits)
-sigma_noise = 1e-2
-sigma_rbf = 0.2
-intsection_allow = False
-nearneighb_n = 5
+    
+def plot_RBF_graphrep(x, number_of_hits, hit_coords, RBF_matrix):
+    H = nx.Graph()
+    hit_coords_dict = {i: tuple(hit_coords[i]) for i in range(number_of_hits)}
+    rbf_edge_weights = []
+    
+    for i in range(number_of_hits):
+        for j in range(i+1, number_of_hits):
+            rbf_edge_weight = RBF_matrix[i][j]
+            H.add_edge(i, j, weight=rbf_edge_weight)
+            rbf_edge_weights.append(rbf_edge_weight)
+            
+            
+    rbf_edge_weights = np.asarray(rbf_edge_weights)
+    
+    edge_contrasts = 0.05 + 0.95 * (rbf_edge_weights - rbf_edge_weights.min()) / (rbf_edge_weights.max() - rbf_edge_weights.min())
 
-track1, track1_labels, track2, track2_labels = construct_toytracks(x, num_hits, sigma_noise, intsection_allow)
-plot_toytracks(x, track1, track1_labels, track2, track2_labels, sigma_noise, intsection_allow)
+        
+    nx.draw_networkx_edges(H, hit_coords_dict, edgelist=H.edges(), alpha=edge_contrasts, edge_color='black')
+    nx.draw_networkx_nodes(H, hit_coords_dict, node_size=200)
+    nx.draw_networkx_labels(H, hit_coords_dict)
+       
+    for detector_x in x:
+        plt.axvline(detector_x, linestyle='--', alpha=0.08)
+    
+    plt.title('RBF Graph')
+    plt.savefig('plots/RBF_Graph.png')
+    plt.show()
 
-hit_coords = np.column_stack([np.concatenate([x, x]),np.concatenate([track1, track2])])
 
-RBF_matrix = construct_RBFmatrix(hit_coords, sigma_rbf)
-plot_matrix_heat_map(RBF_matrix, 'Radial Basis Function')
 
-KNN_matrix = KNN(x, hit_coords, nearneighb_n, track1_labels, track2_labels)
+def main():
+    track_hits = 6
+    x = np.linspace(0,1,track_hits)
+    sigma_noise = 1e-2
+    sigma_rbf = 0.2
+    intsection_allow = False
+    nearneighb_n = 3
 
-plot_matrix_heat_map(KNN_matrix, f'{nearneighb_n}_NearestNeighbours')
+    track1, track1_labels, track2, track2_labels = construct_toytracks(x, track_hits, sigma_noise, intsection_allow)
+    plot_toytracks(x, track1, track1_labels, track2, track2_labels, sigma_noise, intsection_allow)
+
+    hit_coords = np.column_stack([np.concatenate([x, x]),np.concatenate([track1, track2])])
+    number_of_hits = len(hit_coords)
+    
+    RBF_matrix = construct_RBFmatrix(hit_coords, sigma_rbf)
+    plot_matrix_heat_map(RBF_matrix, 'Radial Basis Function')
+    plot_RBF_graphrep(x, number_of_hits, hit_coords, RBF_matrix)
+
+    KNN_matrix, nbrs = contruct_KNN_matrix(x, hit_coords, nearneighb_n, track1_labels, track2_labels, sigma_rbf)
+    plot_matrix_heat_map(KNN_matrix, f'{nearneighb_n}_NearestNeighbours')
+    plot_KNN_graphrep(x, number_of_hits, nbrs, hit_coords)
+    
+    
+main()
