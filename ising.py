1aeea5c6ee0969753fb0a64dd1e26c46d7e9f512
AlexanderCWarwick <Alexander.H.Cook@warwick.ac.uk>
2026-06-23 15:54:10 +0100

    Reorganise into modules

---
+++ ising.py
@@ -0,0 +1,117 @@
+import numpy as np
+from sklearn.metrics.cluster import adjusted_rand_score
+from itertools import product
+
+#######################     Brute force Ising minimisation        ####################### 
+
+def ising_energy(bitstring : np.ndarray[int], W : np.ndarray[np.float64], lambda_bal : float):
+    '''
+    Ising objective function. The function rewards like spins and discourages extreme configurations with a penalty term e.g. 111111111111.
+    Note that the approach here is to use brute force since there is only 12 hits. If n = 20 then brute force would be VERY inefficient.
+    '''
+    isingstring = (2 * bitstring) - 1                   #Convert the bitstring (configuration) into a ising spin configuration.
+    n = len(W)
+    H = 0
+
+    for i in range(n):
+        for j in range(i+1, n):                                         #j > i in Hamiltonian. Avoids double counting the interacting spins.
+            H -= W[i][j] * isingstring[i] * isingstring[j]              #Rewarding term for like spins
+            
+    H += ising_penalty_term(lambda_bal, isingstring)                    #Penalty term penalising big clustering.
+    
+    return H
+
+
+def ising_penalty_term(lambda_bal, isingstring):
+    '''
+    Type of penalty term to be used in the Ising energy. Should be non-negative.
+    Types: 
+    1. lambda_bal * (np.sum(isingstring))**2  
+    
+    2. lambda_bal * (mod(np.sum(isingstring)))
+    3. lambda_bal * (np.sum(isingstring))**4
+    4. lambda_bal * (sum_(i<j)(isingstring))**2
+    '''
+    
+    return lambda_bal * (np.sum(isingstring))**2
+
+
+
+def get_ising_energies(W, lambda_bal, config_space):
+    '''
+    Returns the 'energy landscape' of the the chosen hamiltonian. 
+    '''
+    energies = []
+    for bitstring in config_space:
+        energy = ising_energy(bitstring, W, lambda_bal)
+        energies.append(energy)
+
+    return np.array(energies)
+
+
+
+def get_groundstate(energies, groundstate_energy, config_space):
+    '''
+    Returns where the ground state configuration is (the indices) using the energies array.
+    Exchange degeneracy means energy landscape is symmetric. Hence there are at least two ground states.
+    '''
+    groundstates_indices = np.where(np.isclose(energies,groundstate_energy))[0]
+    groundstate_configs = np.array([config_space[i] for i in groundstates_indices])
+
+    return groundstate_configs
+
+
+
+def KNN_RBF_opt(sim_matrix, lambda_bal, config_space):
+    '''
+    Primary function of the Ising optimisation block. Returns:
+    1. The energy landscape 
+    2. The calculated ground state energy
+    3. The ground state configurations
+    '''
+    
+    energies = get_ising_energies(sim_matrix, lambda_bal, config_space)
+    groundstate_energy = min(energies)  
+    
+    return energies, groundstate_energy, get_groundstate(energies, groundstate_energy, config_space)     
+
+
+            
+def ising_optimisation(number_of_hits, lambda_bal, KNN_matrix, RBF_matrix):
+    
+    binary_config_space = np.array(list(product([0,1], repeat=number_of_hits)))             #List of all 2^12 possible BINARY label configurations. 
+        
+    KNN_energies, KNN_groundstate_energy, KNN_groundstate_binary_configs = KNN_RBF_opt(KNN_matrix, lambda_bal, binary_config_space)
+    RBF_energies, RBF_groundstate_energy, RBF_groundstate_binary_configs = KNN_RBF_opt(RBF_matrix, lambda_bal, binary_config_space)
+            
+        
+    return KNN_energies, KNN_groundstate_energy, KNN_groundstate_binary_configs, RBF_energies, RBF_groundstate_energy, RBF_groundstate_binary_configs
+        
+
+#######################     ARI calculation and check       ####################### 
+
+
+def ARI_check(true_groundstate, KNN_groundstate_binary_configs, RBF_groundstate_binary_configs):
+    '''
+    Adjusted random score measures randomness of the cluster labels. It compares the computed groundstate and the true answer
+    and returns: 
+    ARI = 1 - Perfect clustering (what we are aiming for)
+    0 < ARI < 1 - Random clustering
+    ARI < 0 - Something has gone wrong
+    
+    Test the optimised groundstates against ONLY ONE of the true groundstate tracks, here called true_track.
+    Hence double loop (ising_simmatrix_optimisation())
+    '''
+    KNN_aris = []
+    RBF_aris = []
+        
+    for KNN_state, RBF_state in zip(RBF_groundstate_binary_configs, RBF_groundstate_binary_configs):
+        KNN_aris.append(adjusted_rand_score(true_groundstate, KNN_state))
+        RBF_aris.append(adjusted_rand_score(true_groundstate, RBF_state))
+        
+    KNN_ind = np.argmax(np.array(KNN_aris))
+    RBF_ind = np.argmax(np.array(RBF_aris))
+        
+    print(f'Best KNN clustering is: {KNN_groundstate_binary_configs[KNN_ind]} with ARI = {KNN_aris[KNN_ind]}')
+    print(f'Best RBF clustering is: {RBF_groundstate_binary_configs[RBF_ind]} with ARI = {RBF_aris[RBF_ind]}')
+    
---
+++ plotting.py
@@ -0,0 +1,121 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+def plot_toytracks(x, track1, track2, sigma_noise, allow_intsection):
+    
+    plt.scatter(x, track1, c='blue', s=40, marker='o')
+    plt.scatter(x, track2, c='red', s=40, marker='o')
+    plt.xlim(-0.1, 1.1)
+    plt.title(f'Two track plot with intersection={allow_intsection}. Noise standev.={sigma_noise}')
+    plt.grid(axis='x')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    #plt.savefig('plots/Toytracks.png')
+    plt.show()
+    
+##############################################################################################################################
+
+def plot_similaritymatrix_heatmap(sim_matrix,  matrix_type : str):
+    '''
+    Plot heat map representation of any input similarity matrix. The more correlated hits i and j are, the brighter the (ij)th 
+    coordinate in the heat map.
+    '''
+    plt.imshow(sim_matrix, cmap='viridis')
+    plt.title(f'{matrix_type} Heat map')
+    plt.colorbar()
+    plt.xlabel('Hit index')
+    #plt.savefig(f'plots/{matrix_type}_heat_map.png')
+    plt.show()
+     
+##############################################################################################################################  
+
+def construct_RBF_graphrep(x, number_of_hits,  hit_coords_dict : dict,  RBF_matrix : np.ndarray[np.float64]):
+    '''
+    Create nodes and edges of RBF Graph representation. Edges are weighted and this is shwon via contrast (labels 
+    would be too cluttered). A higher contrast means a smaller similarity.
+    '''
+    H = nx.Graph()
+    rbf_edge_weights = []
+    
+    for i in range(number_of_hits):                         
+        for j in range(i+1, number_of_hits):                       #Similarity matrix must be symmetric hence loop over the upper triangle of the matrix.
+            rbf_edge_weight = RBF_matrix[i][j]
+            
+            H.add_edge(i, j, weight=rbf_edge_weight)               #Add weighted edge to graph instance H.
+            rbf_edge_weights.append(rbf_edge_weight)                
+            
+    rbf_edge_weights = np.asarray(rbf_edge_weights)                #rbf_edge_weights is the ordered list of RBF matrix weights.
+    
+    edge_contrasts = get_edge_contrasts(rbf_edge_weights)          #Convert the RBF weights into contrasts for graph edges. Only for visualisation.
+    plot_graphrep(H, x, hit_coords_dict, H.edges(), edge_contrasts, 'RBF')
+    
+    
+    
+def get_edge_contrasts(rbf_edge_weights : np.ndarray[np.float64])  ->  np.ndarray[np.float64]:
+    '''
+    Contrast parameter alpha must be between 0 and 1. Formula is a linear normalisation.
+    '''
+    return 0.05 + 0.95 * (rbf_edge_weights - rbf_edge_weights.min()) / (rbf_edge_weights.max() - rbf_edge_weights.min())
+
+
+
+def construct_KNN_graphrep(x, number_of_hits, hit_coords, hit_coords_dict,  nbrs):
+    '''
+    KNN similarity is discrete so no contrast. Function obtains 2D array with each hits k nearest neighbours. 
+    keighbors command returns indices including the hit itself. Hence the first index (closest, being the hit itself)
+    is cut off from the array.
+    '''
+    H = nx.Graph()
+    
+    _, indices = nbrs.kneighbors(hit_coords)                #Can ignore distances here since KNN matrix is a discrete metric.
+    indices = indices[:,1:]                                 #Slicing first column to remove self-similarity nodes.
+    
+    for i in range(number_of_hits):
+        for j in indices[i]:
+            H.add_edge(i,j)
+    
+    plot_graphrep(H, x, hit_coords_dict, H.edges(), None, 'KNN')    #No edge_contrasts needed for KNN matrix.
+  
+  
+def plot_graphrep(H, x, hit_coords_dict, edges, edge_contrasts, matrix_type):
+    '''
+    Given the hit_coords dictionary, the edges and edge contrasts (for RBF matrix), plot Graph.'''
+    
+    
+    nx.draw_networkx_edges(H, hit_coords_dict, edgelist=edges, alpha=edge_contrasts, edge_color='black')        #Draw  edges
+    nx.draw_networkx_nodes(H, hit_coords_dict, node_size=200)                                                   #Draw nodes
+    nx.draw_networkx_labels(H, hit_coords_dict)                                                                 #Draw hit labels.
+      
+    for detector_x in x:                                        #Shows positions of the detectors.
+        plt.axvline(detector_x, linestyle='--', alpha=0.12)
+    
+    plt.title(f'{matrix_type} Graph Representation')
+    #plt.savefig(f'plots/{matrix_type}_Graph.png')
+    plt.show()
+
+##############################################################################################################################
+
+
+def plot_energy_landscape(lambda_bal, KNN_energies, RBF_energies): 
+    
+    fig, ax = plt.subplots(2, 2, figsize=(10,6))  
+    ax[0,0].plot(np.sort(KNN_energies), color='orange')
+    ax[0,0].set_title('KNN all states')
+    
+    ax[0,1].plot(np.sort(RBF_energies), color='red')
+    ax[0,1].set_title('RBF all states')
+    
+    ax[1,0].plot(np.sort(KNN_energies)[:10], color='orange')
+    ax[1,0].set_title('KNN lowest 10 energy states')
+    ax[1,0].set_xlabel('Rank')
+    
+    ax[1,1].plot(np.sort(RBF_energies)[:10], color='red')
+    ax[1,1].set_title('RBF lowest 10 energy states')
+    ax[1,1].set_xlabel('Rank')
+    
+    fig.suptitle(f'Energy landscapes for lambda={lambda_bal}')
+    fig.supylabel('Energy')
+    plt.tight_layout()
+    #plt.savefig(f'plots/energy_landscape_λ={lambda_bal}.png')
+    plt.show()
